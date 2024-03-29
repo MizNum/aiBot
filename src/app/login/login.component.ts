59c4d3b6d86a68a6563e9e716b24e828359d6175
Akash Choudhary <choudahry98akash>
2024-03-29 05:59:41 +0530

    Initial basic structure is done

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  constructor(formModule : FormsModule){}
+email ='';
+code ='';
+
+}
