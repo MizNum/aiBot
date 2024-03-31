c254ef2001982839d7a9702a22f13fecebb96963
Faisal Mizzan <faisal.mizzan@gmail.com>
2024-03-31 15:52:36 +0530

    Added login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(formModule : FormsModule){}
-email ='';
-code ='';
+
+constructor(private router: Router) { }
+
+
+  email = '';
+  code = '';
+  recievedCode ='';
+  loginPage = true;
+  otpSent = false;
+
+
+  openHome() {
+    this.recievedCode = '989898';//Use the service send otp to store the code
+    console.log("Otp sent is ", " and the recieved code is ", this.code);
+    if (this.code === '989898') {
+      this.router.navigate(['/home']);
+    }
+    else {
+      //notify by popUP that opt is not verified try again
+    }
+  }
+
+  //on send otp a mail to be sent to required witht a 6 digit otp
+  sendOtp() {
+    //sendOtp
+    this.code = '989898';//replace this with service sendOtp
+    this.otpSent = true;
+
+  }
+
+  onSingnUp() {
+    this.loginPage = !this.loginPage;
+    this.otpSent = false;
+  }
 
 }
